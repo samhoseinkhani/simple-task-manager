897608c32e52f8aeb3b29cf54543f395c797a882
hessamhoseinkhani <hessam199700@gmail.com>
2019-07-02 21:37:14 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Radium from 'radium';
+import { Container, Navbar } from 'react-bootstrap';
+import NavBar from './components/NavBar';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+
     </div>
   );
 }
 
-export default App;
+export default Radium(App);
